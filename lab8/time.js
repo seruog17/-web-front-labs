1a6fa436f4459af841697f3bf6c96d1753d27842
Александра Сарагашева <posagennikov17@gmail.com>
2024-06-02 02:29:57 +0700

    Время

---
+++ lab8/time.js
@@ -0,0 +1,6 @@
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval(showTime, 1000);
